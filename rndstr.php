0f0d8456f495d65359c789677ec99ba939d5074e
marcuslommel <marcus.l@gmx.net>
2017-06-23 20:12:47 +0200

    Add files via upload

---
+++ rndstr.php
@@ -0,0 +1,8 @@
+function rndstr($laenge) 
+{
+	$str = "abcdefghijklmnopqrstuvwxyz0123456789";
+	for($i=0; $i < $laenge; $i++) {
+		$res .= substr($str, mt_rand(0, strlen($str)), 1);	
+	}
+	return $res;
+}
